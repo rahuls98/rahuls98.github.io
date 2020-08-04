3e110082095eb376ef816f223793f311ade210e2
rahuls98 <rahs98@gmail.com>
2020-08-04 20:44:24 +0530

    Sidenav

---
+++ Photography/app.js
@@ -0,0 +1,19 @@
+var sidenav_toggle = document.getElementById("sidenav_toggle");
+var sidenav_close = document.getElementById("sidenav_close");
+var sidenav = document.getElementById("sidenav");
+let sidenav_item = document.getElementsByClassName('sidenav_item')
+
+sidenav_toggle.addEventListener('click', () => {
+    sidenav.style.width = '60vw';
+    sidenav.style.transition = '0.5s';
+    document.getElementById('main-content').style.opacity = '0.5';
+    document.getElementById('main-content').style.transition = '0.8s';
+    for(let key in sidenav_item) sidenav_item[key].style.display = 'block'
+});
+sidenav_close.addEventListener('click', () => {
+    sidenav.style.width = '0vw';
+    sidenav.style.transition = '0.5s';
+    document.getElementById('main-content').style.opacity = '1';
+    document.getElementById('main-content').style.transition = '0.8s';
+    for(let key in sidenav_item) sidenav_item[key].style.display = 'none'
+});
